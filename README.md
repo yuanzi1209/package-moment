835f30d455354d4b36779f06b398c19a27cc0a8f
yuanzi <yuanzi1209@gmail.com>
2021-04-06 19:21:06 +0800

    Upload

---
+++ README.md
@@ -0,0 +1,15 @@
+## Install
+````
+npm install package-moment
+````
+
+## Usage
+````js
+const moment=require('package-moment')
+const dt=moment.dateFormat(new Date())
+// 2021-04-06 19:09:31
+console.log(dt)
+````
+
+## License
+MIT
---
+++ index.js
@@ -0,0 +1,16 @@
+function dateFormat(date) {
+    const dt = new Date(date)
+    
+    const y = dt.getFullYear()
+    const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+    const d = (dt.getDate() + '').padStart(2, '0')
+    const hh = (dt.getHours() + '').padStart(2, '0')
+    const mm = (dt.getMinutes() + '').padStart(2, '0')
+    const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+    return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+}
+
+module.exports = {
+    dateFormat
+}
